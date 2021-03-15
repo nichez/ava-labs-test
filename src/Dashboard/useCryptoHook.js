6b902d34e0b5fd54e2f2354b302c44aeee4af149
nichez <nikola.panev93@gmail.com>
2021-03-15 14:44:35 +0100

    update

--- src/Dashboard/useCryptoHook.js
+++ src/Dashboard/useCryptoHook.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-export default function useBookSearch(pageNumber) {
+export default function useCryptoHook(pageNumber) {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
   const [crypto, setCrypto] = useState([]);
